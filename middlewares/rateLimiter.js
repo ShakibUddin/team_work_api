d6aec31e562dc8a6c7b0391318ffb08201929423
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2024-01-24 23:54:12 +0600

    updated rate limited

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 20, // maximum number of requests allowed in the windowMs
+  max: 60, // maximum number of requests allowed in the windowMs
   message: "Too many requests, please try again later.",
 });
 
